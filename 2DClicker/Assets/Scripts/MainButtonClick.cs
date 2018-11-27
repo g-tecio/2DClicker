179bdb40bad1dc3800ae640ea18ad637d51df980
Luis J. Reyes R <41127806+LuisReyesGTEC@users.noreply.github.com>
2018-11-27 16:44:59 -0600

    First Version
    First Version of 2D Clicker

---
+++ 2DClicker/Assets/Scripts/MainButtonClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainButtonClick : MonoBehaviour {
+
+    public GameObject textBox;
+
+
+
+    public void ClickTheButton (){
+
+        GlobalCookies.CookieCount += 1;
+
+    }
+     
+}
---
+++ 2DClicker/Assets/Scripts/PurcheaseLog.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurcheaseLog : MonoBehaviour {
+
+    public GameObject AutoCookie;
+
+    public void StartAutoCookie (){
+        AutoCookie.SetActive(true);
+        GlobalCash.CashCount -= GlobalBaker.bakerValue;
+        GlobalBaker.bakerValue *= 2;
+        GlobalBaker.turnOffButton = true;
+        GlobalBaker.bakePerSec += 1;
+        GlobalBaker.numberOfBaker += 1;
+    }
+}
